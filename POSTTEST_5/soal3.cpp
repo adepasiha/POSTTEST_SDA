c1f4c24f57d76a8e40a7988dfa047d1c982b4975
Ade ganteng <pasihaade@gmail.com>
2025-10-14 20:02:03 +0800

    Add files via upload

---
+++ POSTTEST_5/soal3.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+Node* insert(Node* root, int val) {
+    if (root == nullptr) {
+        return new Node(val);
+    }
+    if (val < root->data) {
+        root->left = insert(root->left, val);
+    } else if (val > root->data) {
+        root->right = insert(root->right, val);
+    }
+    return root;
+}
+
+int findMaxValue(Node* root) {
+    // Kalau tree kosong
+    if (root == nullptr)
+        return -1;
+
+    // Selama masih punya anak kanan, lanjut ke kanan
+    while (root->right != nullptr) {
+        root = root->right;
+    }
+
+    // Node paling kanan adalah nilai terbesar
+    return root->data;
+}
+
+int main() {
+    Node* root = nullptr;
+    root = insert(root, 50);
+    insert(root, 30);
+    insert(root, 70);
+    insert(root, 20);
+    insert(root, 80);
+
+    cout << "Nilai terbesar dalam tree adalah: " << findMaxValue(root) << endl; 
+    // Output: 80
+    return 0;
+}
---
+++ POSTTEST_5/soal4.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+Node* insert(Node* root, int val) {
+    if (root == nullptr) {
+        return new Node(val);
+    }
+    if (val < root->data) {
+        root->left = insert(root->left, val);
+    } else if (val > root->data) {
+        root->right = insert(root->right, val);
+    }
+    return root;
+}
+
+void postOrderTraversal(Node* root) {
+    if (root == nullptr)
+        return;
+
+    // Kiri -> Kanan -> Root
+    postOrderTraversal(root->left);
+    postOrderTraversal(root->right);
+    cout << root->data << " ";
+}
+
+int main() {
+    Node* root = nullptr;
+    root = insert(root, 50);
+    insert(root, 30);
+    insert(root, 70);
+    insert(root, 20);
+    insert(root, 40);
+    insert(root, 60);
+    insert(root, 80);
+
+    cout << "Post-order traversal dari tree adalah: ";
+    postOrderTraversal(root);
+    // Output: 20 40 30 60 80 70 50
+    cout << endl;
+    return 0;
+}
